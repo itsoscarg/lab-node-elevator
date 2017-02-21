4842ac328dc165e8e18d282c0cd1328cecd8c9fc
Oscar Guerrero <itsoscarg@icloud.com>
2017-02-21 08:58:52 -0500

    this was a very hard assignment

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,33 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+
+    update() {
+      "The status of the elevator is: " + this.log,
+  };
+
+    log() {
+      if (floorUp > this.floor) {
+      console.log("Direction: Going Up");
+      else
+      console.log("Direction: Going Down")
+      }
+
+      "Floor: " + this.floor
+    }
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
+
+
+  start() { } //setInterval,  calls Update every second and eventually calls log.
+  stop() { } //clears the Interval
+
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,33 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const myElevator  = new Elevator();
+
+const mary = new Person('Mary', 2, 8);
+const sabina = new Person('Sabina', 7, 1);
+const fernanda = new Person('Fernanda', 4, 3);
+
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.floorDown();
+myElevator.log();
+
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.floorUp();
+myElevator.log();
